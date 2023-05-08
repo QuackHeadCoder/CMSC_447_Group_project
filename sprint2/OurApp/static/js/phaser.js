1ec97e0a7241dae594b90a914adc7a0d50d3819a
T.Pham <thanhp1@umbc.edu>
2023-05-07 21:58:30 -0400

    added lvl 3 template

--- sprint2/OurApp/static/js/phaser.js
+++ sprint2/OurApp/static/js/phaser.js
@@ -9,10 +9,8 @@
  * @todo when game is finished it should make a PUT request to update user information
  */
 
-
 //for API get/put calls
-url = "http://127.0.0.1:5000/"
-
+url = "http://127.0.0.1:5000/";
 
 var playerData = null;
 /**
@@ -20,82 +18,69 @@ var playerData = null;
  * null otherwise
  **/
 async function getData() {
-
   //attempts to get data
   try {
-    response = await fetch(url+"get");
+    response = await fetch(url + "get");
     jsonData = await response.json();
     let id = jsonData["id"];
     let username = jsonData["username"];
     let password = jsonData["password"];
     let currentLevel = jsonData["currentLevel"];
     let topScore = jsonData["topScore"];
-    playerData ={id,username,password,currentLevel,topScore};
-    return playerData
-  }
-
-  //catches error if user is not currently logged in
-  catch (error) {
+    playerData = { id, username, password, currentLevel, topScore };
+    return playerData;
+  } catch (error) {
+    //catches error if user is not currently logged in
     return null;
   }
 }
 
-/** 
+/**
  * Update user will only take in current level and top score values
  * If top score is not changed then we just ignore and return null
-**/
-async function updateUser(currentLevel,topScore){
+ **/
+async function updateUser(currentLevel, topScore) {
   //get data, if no data is loaded we quit
   data = await getData();
-  if(data == null)return null;
+  if (data == null) return null;
 
   //if top score does not need to be updated we quit
-  if(topScore <= data["topScore"])
-    return null;
+  if (topScore <= data["topScore"]) return null;
 
   data["currentLevel"] = currentLevel;
   data["topScore"] = topScore;
 
   //data packet that needs to be sent
-  const response = await fetch(url+"api/update_user", {
-    method: 'PUT',
+  const response = await fetch(url + "api/update_user", {
+    method: "PUT",
     headers: {
-      'Content-type': 'application/json'
+      "Content-type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
 
   //simple logging of response
   const ret = await response;
   console.log(ret);
-  return ret
+  return ret;
 }
 /*
 A simple example on how to use the get Data function
 As well as how to update User
 Delete this when to reduce console logs
 */
-async function test(){
-
+async function test() {
   //since it is async it needs an await call to wait for the response from the server
   //then once it is stored we can manipulate the data in which way we want such as printing out the id
   data = await getData();
-  console.log(data['currentLevel']);
+  console.log(data["currentLevel"]);
 
   //updates user level,score
-  updateUser(3,15);
+  updateUser(3, 15);
   data = await getData();
-  console.log(data['currentLevel']);
+  console.log(data["currentLevel"]);
 }
 test();
-
-
-
-
-
-
-
-
 
 function meteors(scene, meteor_key) {
   // private
@@ -310,6 +295,7 @@ function player(scene, start = { x: 20, y: 50 }, key) {
   this.speed = 200;
   this.frame_rate = 30;
   this.key = key;
+  this.invincible = false;
   this.player.setBounce(0.2);
   this.player.setCollideWorldBounds(true);
   this.player.setOffset(8, 12);
@@ -346,6 +332,17 @@ function player(scene, start = { x: 20, y: 50 }, key) {
   const moveY = (top, bottom) => {
     this.player.setVelocityY((bottom - top) * this.speed);
   };
+
+  const isInvincible = () => {
+    return this.invincible;
+  };
+  const set_invincible = (invincible) => {
+    if (typeof invincible === "boolean") {
+      this.invincible = invincible;
+    } else {
+      this.invincible = false;
+    }
+  };
   return {
     get_player,
     set_player,
@@ -357,6 +354,8 @@ function player(scene, start = { x: 20, y: 50 }, key) {
     set_frame_rate,
     moveX,
     moveY,
+    isInvincible,
+    set_invincible,
   };
 }
 
@@ -430,11 +429,6 @@ var config = {
       debug: false,
     },
   },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
 };
 /* CONSTANT */
 
@@ -442,6 +436,7 @@ var config = {
 const meteor_max_scale = 4;
 const level1scenekey = "level1Scene";
 const level2scenekey = "level2Scene";
+const level3scenekey = "level3Scene";
 
 /* END OF CONSTANT */
 
@@ -491,7 +486,98 @@ function hitBomb(player, bomb) {
 
 // end of support functions
 /*Preload anything needed for all scenes here */
-function preload() {
+// function preload() {
+//   console.log(this);
+//   this.load.image("sky", "../static/js/assets/sky.png");
+//   this.load.image("ground", "../static/js/assets/platform.png");
+//   this.load.image("bomb", "../static/js/assets/bomb.png");
+//   this.load.image("star", "../static/js/assets/star.png");
+//   this.load.image("night", "../static/js/assets/level2night.webp");
+//   this.load.image("level2ground", "../static/js/assets/emptyplatform.png");
+//   this.load.image("level2platform", "../static/js/assets/level2platform.png");
+//   this.load.spritesheet("player", "../static/js/assets/dude.png", {
+//     frameWidth: 32,
+//     frameHeight: 48,
+//   });
+//   this.load.audio("gaming_music", ["../static/js/assets/hey_ya.mp3"]);
+
+//   //Progress bar
+
+//   //creating box and bar and some coloring
+//   var progressBar = this.add.graphics();
+//   var progressBox = this.add.graphics();
+//   progressBox.fillStyle(0x222222, 0.8);
+//   progressBox.fillRect(240, 270, 320, 50);
+
+//   //adjusting dimensions
+//   var width = this.cameras.main.width;
+//   var height = this.cameras.main.height;
+//   var loadingText = this.make.text({
+//     x: width / 2,
+//     y: height / 2 - 50,
+//     text: "Loading...",
+//     style: {
+//       font: "20px monospace",
+//       fill: "#000000",
+//     },
+//   });
+//   loadingText.setOrigin(0.5, 0.5);
+
+//   var percentText = this.make.text({
+//     x: width / 2,
+//     y: height / 2 - 5,
+//     text: "0%",
+//     style: {
+//       font: "18px monospace",
+//       fill: "#ffffff",
+//     },
+//   });
+//   percentText.setOrigin(0.5, 0.5);
+
+//   var assetText = this.make.text({
+//     x: width / 2,
+//     y: height / 2 + 50,
+//     text: "",
+//     style: {
+//       font: "18px monospace",
+//       fill: "#ffffff",
+//     },
+//   });
+//   assetText.setOrigin(0.5, 0.5);
+
+//   //setting value of progress bar and to be placed in the bar
+//   this.load.on("progress", function (value) {
+//     percentText.setText(parseInt(value * 100) + "%");
+//     progressBar.clear();
+//     progressBar.fillStyle(0xffffff, 1);
+//     progressBar.fillRect(250, 280, 300 * value, 30);
+//   });
+
+//   //setting value of asset loading to be placed under bar
+//   this.load.on("fileprogress", function (file) {
+//     assetText.setText("Loading asset: " + file.key);
+//   });
+
+//   //simple cleanup
+//   this.load.on("complete", function () {
+//     progressBar.destroy();
+//     progressBox.destroy();
+//     loadingText.destroy();
+//     percentText.destroy();
+//     assetText.destroy();
+//   });
+// }
+/*LEVEL 1 CODE BEGINS */
+function create() {}
+
+function update() {
+  this.scene.start(level1scenekey);
+}
+
+/* LEVEL 1 CODE ENDS */
+var level1Scene = new Phaser.Scene(level1scenekey);
+
+level1Scene.preload = function () {
   console.log(this);
   this.load.image("sky", "../static/js/assets/sky.png");
   this.load.image("ground", "../static/js/assets/platform.png");
@@ -504,7 +590,7 @@ function preload() {
     frameWidth: 32,
     frameHeight: 48,
   });
-  this.load.audio("gaming_music",["../static/js/assets/hey_ya.mp3"]);
+  this.load.audio("gaming_music", ["../static/js/assets/hey_ya.mp3"]);
 
   //Progress bar
 
@@ -513,85 +599,72 @@ function preload() {
   var progressBox = this.add.graphics();
   progressBox.fillStyle(0x222222, 0.8);
   progressBox.fillRect(240, 270, 320, 50);
-  
+
   //adjusting dimensions
   var width = this.cameras.main.width;
   var height = this.cameras.main.height;
   var loadingText = this.make.text({
-      x: width / 2,
-      y: height / 2 - 50,
-      text: 'Loading...',
-      style: {
-          font: '20px monospace',
-          fill: '#000000'
-      }
+    x: width / 2,
+    y: height / 2 - 50,
+    text: "Loading...",
+    style: {
+      font: "20px monospace",
+      fill: "#000000",
+    },
   });
   loadingText.setOrigin(0.5, 0.5);
-  
+
   var percentText = this.make.text({
-      x: width / 2,
-      y: height / 2 - 5,
-      text: '0%',
-      style: {
-          font: '18px monospace',
-          fill: '#ffffff'
-      }
+    x: width / 2,
+    y: height / 2 - 5,
+    text: "0%",
+    style: {
+      font: "18px monospace",
+      fill: "#ffffff",
+    },
   });
   percentText.setOrigin(0.5, 0.5);
-  
+
   var assetText = this.make.text({
-      x: width / 2,
-      y: height / 2 + 50,
-      text: '',
-      style: {
-          font: '18px monospace',
-          fill: '#ffffff'
-      }
+    x: width / 2,
+    y: height / 2 + 50,
+    text: "",
+    style: {
+      font: "18px monospace",
+      fill: "#ffffff",
+    },
   });
   assetText.setOrigin(0.5, 0.5);
-  
+
   //setting value of progress bar and to be placed in the bar
-  this.load.on('progress', function (value) {
-      percentText.setText(parseInt(value * 100) + '%');
-      progressBar.clear();
-      progressBar.fillStyle(0xffffff, 1);
-      progressBar.fillRect(250, 280, 300 * value, 30);
+  this.load.on("progress", function (value) {
+    percentText.setText(parseInt(value * 100) + "%");
+    progressBar.clear();
+    progressBar.fillStyle(0xffffff, 1);
+    progressBar.fillRect(250, 280, 300 * value, 30);
   });
-  
+
   //setting value of asset loading to be placed under bar
-  this.load.on('fileprogress', function (file) {
-      assetText.setText('Loading asset: ' + file.key);
+  this.load.on("fileprogress", function (file) {
+    assetText.setText("Loading asset: " + file.key);
   });
 
   //simple cleanup
-  this.load.on('complete', function () {
-      progressBar.destroy();
-      progressBox.destroy();
-      loadingText.destroy();
-      percentText.destroy();
-      assetText.destroy();
+  this.load.on("complete", function () {
+    progressBar.destroy();
+    progressBox.destroy();
+    loadingText.destroy();
+    percentText.destroy();
+    assetText.destroy();
   });
-
-}
-/*LEVEL 1 CODE BEGINS */
-function create() {
-
-}
-
-function update() {
-    this.scene.start(level1scenekey);
-}
-
-/* LEVEL 1 CODE ENDS */
-var level1Scene = new Phaser.Scene(level1scenekey);
-
-level1Scene.create = function() {
-    //music
-  music = this.sound.add("gaming_music",{loop:true,volume:0.5});
+};
+level1Scene.create = function () {
+  //music
+  music = this.sound.add("gaming_music", { loop: true, volume: 0.5 });
   music.play();
 
   this.add.image(400, 300, "sky");
-  mscore = score();
+  // mscore = score();
   // Display Score at Start of Game
   scoreText = this.add.text(20, 20, "", {
     font: "16px Courier",
@@ -687,13 +760,7 @@ level1Scene.create = function() {
   this.physics.add.collider(mplayer.get_player(), platforms);
 
   // Add collision between player and bombs, end game if collision happens
-  this.physics.add.collider(
-    mplayer.get_player(),
-    bombs.get_meteors(),
-    hitBomb,
-    null,
-    this
-  );
+  this.physics.add.collider(mplayer, bombs.get_meteors(), hitBomb, null, this);
 
   // add text object to the game
   let text = this.add.text(400, 300, "", {
@@ -721,6 +788,7 @@ level1Scene.create = function() {
       } else {
         // bonus_invincible = true;
         mscore.set_invincible(true);
+        mplayer.set_invincible(true);
         text.setText("Invincibility bonus activated!");
       }
     }
@@ -738,6 +806,7 @@ level1Scene.create = function() {
           mbonuses.set_cur_bonus("");
           text.setText("");
           mplayer.get_player().clearTint();
+          mplayer.set_invincible(false);
         }
       }
     },
@@ -756,25 +825,25 @@ level1Scene.create = function() {
       bonus.destroy();
     }
   );
+};
 
-
-
-}
-
-
-level1Scene.update = function(){
-  if(level2){
+level1Scene.update = function () {
+  if (level2) {
     //show text to player for 2 seconds then start next level!
     nextLevelText = this.add.text(250, 250, "Level 1 Completed!", {
       font: "30px Courier",
       fill: "#FFFFFF",
     });
-    
-    this.time.delayedCall(1500, function() {
-      nextLevelText = "";
-      this.scene.start(level2scenekey);
-    }, [], this);
-    
+
+    this.time.delayedCall(
+      1500,
+      function () {
+        nextLevelText = "";
+        this.scene.start(level2scenekey);
+      },
+      [],
+      this
+    );
   }
   // Player animations based on keyboard inputs
   if (!gameOver && cursors.left.isDown) {
@@ -800,9 +869,7 @@ level1Scene.update = function(){
     level2 = true;
     // bombs.destroy_all();
   }
-}
-
-
+};
 
 /* LEVEL 2 CODE BEGINS*/
 var level2Scene = new Phaser.Scene(level2scenekey);
@@ -922,6 +989,7 @@ level2Scene.create = function () {
       if (start_reset != 0 && Date.now() - start_reset >= 5000) {
         start_reset = 0;
         mscore.reset();
+        mplayer.set_invincible(false);
         mbonuses.set_cur_bonus("");
         text.setText("");
         mplayer.get_player().clearTint();
@@ -996,8 +1064,8 @@ level3Scene.preload = function () {
   this.load.image("bomb", "../static/js/assets/bomb.png");
   this.load.image("star", "../static/js/assets/star.png");
   this.load.image("night", "../static/js/assets/level2night.webp");
-  this.load.image("level2ground", "../static/js/assets/emptyplatform.png");
-  this.load.image("level2platform", "../static/js/assets/level2platform.png");
+  this.load.image("level3ground", "../static/js/assets/emptyplatform.png");
+  this.load.image("level3platform", "../static/js/assets/level2platform.png");
   this.load.spritesheet("player", "../static/js/assets/dude.png", {
     frameWidth: 32,
     frameHeight: 48,
@@ -1020,25 +1088,28 @@ level3Scene.create = function () {
 
   scoreText.setText("Current Score: " + mscore.get_score());
 
-  platforms = this.physics.add.staticGroup();
-  platforms.create(400, 725, "level2ground").setScale(2).refreshBody();
-  platforms.getChildren()[0].setOffset(0, 12);
+  ground = this.physics.add.staticGroup();
+  ground.create(400, 725, "level3ground").setScale(2).refreshBody();
+  ground.getChildren()[0].setOffset(0, 12);
 
-  level2platforms = this.physics.add.staticGroup();
-  level2platforms.create(0, 300, "level2platform").setScale(1).refreshBody();
-  level2platforms.create(750, 300, "level2platform").setScale(1).refreshBody();
-  level2platforms.getChildren()[0].setOffset(0, 12);
+  level3platforms = this.physics.add.staticGroup();
+  level3platforms.create(0, 300, "level3platform").setScale(1).refreshBody();
+  level3platforms.create(750, 300, "level3platform").setScale(1).refreshBody();
+  level3platforms.getChildren()[0].setOffset(0, 12);
 
-  mplayer.set_player(this.physics.add.sprite(16, 450, "player"));
+  // mplayer.set_player(this.physics.add.sprite(16, 450, "player"));
+  mplayer = player(this, { x: 100, y: 400 }, "player");
   // level2 has increase player speed
   mplayer.set_speed(500);
-  bombs.set_meteors(this.physics.add.group());
+  bombs = meteors(this, "bomb");
+  // bombs.set_meteors(this.physics.add.group());
   bombs.set_angle({ min_angle: -10, max_angle: 10 });
   bombs.set_meteors_number(4);
   mplayer.get_player().setGravityY(300);
   mplayer.get_player().setCollideWorldBounds(true);
 
   // Create bonus group
+  mbonuses = bonuses(this);
   mbonuses.set_bonuses(this.physics.add.group());
   mbonuses.set_angle({ min_angle: -30, max_angle: 30 });
   // time event for creating meteor
@@ -1059,7 +1130,6 @@ level3Scene.create = function () {
   });
 
   // Add collision between player and bombs, end game if collision happens
-
   this.physics.add.collider(
     mplayer.get_player(),
     bombs.get_meteors(),
@@ -1067,7 +1137,31 @@ level3Scene.create = function () {
     null,
     this
   );
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers(mplayer.get_key(), {
+      start: 0,
+      end: 3,
+    }),
+    frameRate: mplayer.get_frame_rate(),
+    repeat: -1,
+  });
 
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: mplayer.get_key(), frame: 4 }],
+    frameRate: mplayer.get_frame_rate(),
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers(mplayer.get_key(), {
+      start: 5,
+      end: 8,
+    }),
+    frameRate: mplayer.get_frame_rate(),
+    repeat: -1,
+  });
   this.anims.create({
     key: "up",
     frames: [{ key: mplayer.get_key(), frame: 4 }],
@@ -1077,11 +1171,11 @@ level3Scene.create = function () {
   cursors = this.input.keyboard.createCursorKeys();
 
   // Add collision between player and ground platform
-  this.physics.add.collider(mplayer.get_player(), platforms);
-  this.physics.add.collider(mplayer.get_player(), level2platforms);
-  this.physics.add.collider(mbonuses.get_bonuses(), level2platforms);
+  this.physics.add.collider(mplayer.get_player(), ground);
+  this.physics.add.collider(mplayer.get_player(), level3platforms);
+  this.physics.add.collider(mbonuses.get_bonuses(), level3platforms);
   // Collision between bombs and platforms, destroy bomb and update score
-  this.physics.add.collider(bombs.get_meteors(), platforms, function (bomb) {
+  this.physics.add.collider(bombs.get_meteors(), ground, function (bomb) {
     bomb.destroy();
     mscore.increase_score();
     scoreText.setText("Current Score: " + mscore.get_score());
@@ -1095,10 +1189,10 @@ level3Scene.create = function () {
   text.setOrigin(0.5);
   // collision between bonuses and player
   this.physics.add.collider(
-    mplayer.get_player(),
+    mplayer,
     mbonuses.get_bonuses(),
     function (player, bonus) {
-      player.setTint(bonus.tintTopLeft);
+      player.get_player().setTint(bonus.tintTopLeft);
       bonus.destroy();
       start_reset = Date.now();
       if (mbonuses.get_cur_bonus() === "speed") {
@@ -1112,6 +1206,7 @@ level3Scene.create = function () {
       } else {
         // bonus_invincible = true;
         mscore.set_invincible(true);
+        play.set_invincible(true);
         text.setText("Invincibility bonus activated!");
       }
     }
@@ -1126,18 +1221,15 @@ level3Scene.create = function () {
         mscore.reset();
         mbonuses.set_cur_bonus("");
         text.setText("");
+        mplayer.set_invincible(false);
         mplayer.get_player().clearTint();
       }
     },
   });
 
-  this.physics.add.collider(
-    mbonuses.get_bonuses(),
-    platforms,
-    function (bonus) {
-      bonus.destroy();
-    }
-  );
+  this.physics.add.collider(mbonuses.get_bonuses(), ground, function (bonus) {
+    bonus.destroy();
+  });
 };
 
 level3Scene.update = function () {
@@ -1168,7 +1260,20 @@ level3Scene.update = function () {
 
 // END OF LEVEL 3 CODE
 
-var game = new Phaser.Game(config);
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "game",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: [level3Scene],
+};
 
-game.scene.add(level1scenekey,level1Scene)
-game.scene.add(level2scenekey, level2Scene);
+mscore = score();
+var game = new Phaser.Game(config);
